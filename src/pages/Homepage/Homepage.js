796242332c26d9cf70aaf19dbc9a5f596e2e5a84
Candice Serate <cwserate@gmail.com>
2022-12-13 14:05:19 -0500

    created the movie card container and passed props

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Slider from '../../components/Header/Slider/Slider'
+import MovieCard from '../../components/MovieCard/MovieCard';
 import './Homepage.css'
 import axios from 'axios';
+
 
 function Homepage() {
   const apiKey = process.env.REACT_APP_API_KEY;
@@ -11,20 +13,31 @@ function Homepage() {
     React.useEffect(
       ()=>{
           
-          //call api to get upcoming movies
+          //call api to get popular movies
           axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}&page=1`)
           // https://api.themoviedb.org/3/movie/popular?api_key=d6436bb18d070d5e4de2901cd97c9adf&page=1
           .then(res =>{
-              console.log(res.data.results);
+              // console.log(res.data.results);
               //store data from api into state
               setPopularMovies(res.data.results)
           })
           .catch(err => console.log(err))
-  
+        
+          //call api to get top rated movies
+          axios.get(`${baseUrl}/movie/top_rated?api_key=${apiKey}&page=1`)
+          
+          .then(res =>{
+              console.log(res.data.results);
+              //store data from api into state
+             setTopRatedMovies(res.data.results)
+          })
+          .catch(err => console.log(err))
       }, []
   )
   //create state to hold popular movies
   const [popularMovies, setPopularMovies] = React.useState([])
+  //create state to hold top rated movies
+  const [topRatedMovies, setTopRatedMovies] = React.useState([])
 
   return (
 
@@ -34,15 +47,31 @@ function Homepage() {
           <div className='popular-container'>
             <h3>Popular Movies</h3>
             <div className='popular-cards-wrapper'>
-              {
+              {/* {
                 popularMovies.map(item=> <p>{item.title}</p>)
+              } */}
+              {
+                popularMovies.map(item=> <MovieCard movie={item}
+                                                    imgUrl={item.poster_path}
+                                                    imgheight= "300px"
+                                                    cardStyle= "popular-card"
+                                                    borderRadius= "16px" />)
               }
+              
             </div>
             <div className='page-numbers'>Page Numbers</div>
           </div>
           <div className='top-rated-container'>
             <h3>Top Rated Movies</h3>
-            <div className='top-rated-cards-wrapper'>Top Rated Movies Go Here</div>
+            <div className='top-rated-cards-wrapper'>
+              {
+              topRatedMovies.map(item=> <MovieCard movie={item}
+                                                  imgUrl={item.backdrop_path}
+                                                  imgheight= "100px"
+                                                  cardStyle= "top-rated-card"
+                                                  borderRadius= "8px" />)
+              }
+              </div>
           </div>
         </div>
     </div>
