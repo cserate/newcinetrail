116d2131ec13c769e0d8901d861abc727b3e2926
Candice Serate <cwserate@gmail.com>
2022-12-13 11:08:33 -0500

    will reconfigure ratings later

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -12,6 +12,17 @@ function Homepage() {
    
     <div className='homepage-container'>
         <Slider /> 
+        <div className='movies-wrapper'>
+          <div className='popular-container'>
+            <h3>Popular Movies</h3>
+            <div className='popular-cards-wrapper'>Popular Movies Go Here</div>
+            <div className='page-numbers'>Page Numbers</div>
+          </div>
+          <div className='top-rated-container'>
+            <h3>Top Rated Movies</h3>
+            <div className='top-rated-cards-wrapper'>Top Rated Movies Go Here</div>
+          </div>
+        </div>
     </div>
   )
 }
